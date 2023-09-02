1aeb89eb21619026caf3ab9462c36e6e7fd8a2c7
Jon Craton <jncraton@gmail.com>
2023-09-01 21:45:28 -0400

    Allow easily changing Python location

--- test.py
+++ test.py
@@ -1,5 +1,9 @@
 from subprocess import run, PIPE
 
+# Name of Python interpreter
+# This can be replaced with a full path to Python 3
+python_bin = "python3"
+
 tests = [
     {"inputs": (0, 10, 2), "expects": ("10", "5")},
     {"inputs": (0, 1, 2), "expects": ("1", "0.5")},
@@ -14,7 +18,7 @@ failed = 0
 for test in tests:
     stdin = '\n'.join(map(str, test["inputs"])).encode()
     
-    stdout = run(["python3", "gas.py"], input=stdin, stdout=PIPE).stdout
+    stdout = run([python_bin, "gas.py"], input=stdin, stdout=PIPE).stdout
     
     for expect in test["expects"]:
         if expect.encode() in stdout:
