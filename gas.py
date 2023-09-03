58a60d118f62e35dc6c31ae5dc264aff78f74828
Jon Craton <jncraton@gmail.com>
2023-09-03 18:46:54 -0400

    Add program template

---
+++ gas.py
@@ -0,0 +1,7 @@
+# Request the mileage values
+
+# Display the miles travelled to the user
+
+# Request amount of gas added to the tank on this fill-up
+
+# Display the miles per gallon for this fill-up to the user
